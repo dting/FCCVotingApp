5a1101a0c7f1ee390e4167c9a3101c040db8a4ce
Dennis Ting <dennisting@gmail.com>
2015-07-15 00:39:02 -0700

    Added dashboard and poll routes.

---
+++ client/app/dashboard/dashboard.controller.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('workspaceApp')
+  .controller('DashboardCtrl', function ($scope) {
+    $scope.message = 'Hello';
+  });
---
+++ client/app/dashboard/dashboard.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('workspaceApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('dashboard', {
+        url: '/dashboard',
+        templateUrl: 'app/dashboard/dashboard.html',
+        controller: 'DashboardCtrl'
+      });
+  });
