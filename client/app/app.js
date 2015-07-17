6f5e77333687ad3e10e15800fba0f5cc0a0d4121
Dennis Ting <dennisting@gmail.com>
2015-07-17 01:11:03 -0700

    Add highcharts for visualizations.

--- client/app/app.js
+++ client/app/app.js
@@ -5,7 +5,8 @@ angular.module('workspaceApp', [
   'ngResource',
   'ngSanitize',
   'ui.router',
-  'ui.bootstrap'
+  'ui.bootstrap',
+  'highcharts-ng'
 ])
   .config(function ($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
     $urlRouterProvider
--- client/app/poll/poll.controller.js
+++ client/app/poll/poll.controller.js
@@ -3,18 +3,43 @@
 angular.module('workspaceApp')
   .controller('PollCtrl', function ($scope, $stateParams, Poll, Auth) {
     $scope.vote = {};
+
     Poll.get({id: $stateParams.id}).$promise.then(function(r) {
       $scope.poll = r;
-      $scope.vote.selected = r.selected;
+      if (r.selected) {
+        $scope.vote.selected = r.selected;
+        $scope.chartConfig.series = resultChartData($scope.poll);
+      };
     });
+
     $scope.vote = function() {
       $scope.pending = true;
       Poll.vote({id:$scope.poll._id, choice:$scope.vote.selected}).$promise.then(function(res) {
         $scope.poll = res;
+        $scope.chartConfig.series = resultChartData($scope.poll);
         $scope.pending = false;
       }, function(err) {
         $scope.errors = err;
         $scope.pending = false;
       });
     };
+
+    $scope.currentUserId = function() {
+      return Auth.getCurrentUser()._id;
+    };
+
+    $scope.chartConfig = {
+      options: {
+        chart: {
+          type: 'bar'
+        }
+      },
+      loading: false
+    };
   });
+
+function resultChartData(poll) {
+  return poll.choices.map(function(choice) {
+    return {name: choice.text, data:[choice.votes.length]};
+  });
+}
