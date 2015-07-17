dd0ea7f091184fa99c539cac0d2ba6f577244c90
Dennis Ting <dennisting@gmail.com>
2015-07-17 01:25:12 -0700

    Add color theme to HighCharts.

--- client/app/poll/poll.controller.js
+++ client/app/poll/poll.controller.js
@@ -9,6 +9,7 @@ angular.module('workspaceApp')
       if (r.selected) {
         $scope.vote.selected = r.selected;
         $scope.chartConfig.series = resultChartData($scope.poll);
+        $scope.chartConfig.title = {text:$scope.poll.text};
       };
     });
 
@@ -17,6 +18,7 @@ angular.module('workspaceApp')
       Poll.vote({id:$scope.poll._id, choice:$scope.vote.selected}).$promise.then(function(res) {
         $scope.poll = res;
         $scope.chartConfig.series = resultChartData($scope.poll);
+        $scope.chartConfig.title = {text:$scope.poll.text};
         $scope.pending = false;
       }, function(err) {
         $scope.errors = err;
@@ -32,7 +34,11 @@ angular.module('workspaceApp')
       options: {
         chart: {
           type: 'bar'
-        }
+        },
+        colors: ['#bf616a', '#d08770', '#ebcb8b', '#a3be8c', '#96b5b4', '#8fa1b3', '#FF9655', '#b48ead', '#ab7967']
+      },
+      xAxis: {
+        categories: ['Votes']
       },
       loading: false
     };
